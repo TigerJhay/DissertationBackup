54164d74a99ab66fbb7647bc0a2fcc089bcfada7
TigerJhay <jherold.306@gmail.com>
2024-11-30 18:30:22 +0800

    modified:   anothertest.py 	modified:   app.py 	modified:   templates/index.html 	new file:   templates/template.css 	modified:   test.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-import
+import openai
+
+
+openai.api_key = "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"
+
+completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Give me 3 ideas for apps I could build with openai apis "}])
+
+print(completion.choices[0].message.content)
