923f7ebd2b188cfe69d77bae8184b0acc9381798
TigerJhay <jherold.306@gmail.com>
2024-12-04 23:10:42 +0800

    modified:   __pycache__/views.cpython-311.pyc 	new file:   templates/scratch.txt 	modified:   templates/testscript.html 	modified:   views.py

--- views.py
+++ views.py
@@ -8,6 +8,14 @@ def home():
      return render_template("index.html")
      #return render_template("testscript.html")
 
+
+@views.route("/testflask")
+def testarea2():
+     strmessage1 = "This is text \n inside flash"
+     strmessage2 = "Lorem ipsum dolor sit amet ** , consectetur adipiscing elit **"
+     strmessage3 = "Lorem ipsum dolor sit amet ** <br> , consectetur adipiscing elit ** , sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+     return render_template("testscript.html", message_content = strmessage3)
+     
 @views.route("/testroute")
 def testarea():
      strmessage = "Lorem ipsum dolor sit amet ** , consectetur adipiscing elit ** , sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
