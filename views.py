c12e4c3476f734596b06300c83aa39f9a92e252c
TigerJhay <jherold.306@gmail.com>
2024-12-07 20:08:36 +0800

    modified:   __pycache__/views.cpython-311.pyc 	modified:   templates/testscript.html 	modified:   views.py

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 import google.generativeai as genai
+import html
 
 views = Blueprint(__name__, "views")
 
@@ -14,7 +15,8 @@ def testarea2():
      strmessage1 = "This is text \n inside flash"
      strmessage2 = "Lorem ipsum dolor sit amet ** , consectetur adipiscing elit **"
      strmessage3 = "Lorem ipsum dolor sit amet ** <br> , consectetur adipiscing elit ** , sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-     return render_template("testscript.html", message_content = strmessage3)
+     print(strmessage3)
+     return render_template("testscript.html", message_content = html.escape('\n', '<br>'))
      
 @views.route("/testroute")
 def testarea():
