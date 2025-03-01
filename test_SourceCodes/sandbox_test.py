e7ba7442fd56633868e591d732741bb8732a76b1
TigerJhay <jherold.306@gmail.com>
2025-03-01 17:53:14 +0800

    modified:   test_SourceCodes/sandbox_test.py

--- test_SourceCodes/sandbox_test.py
+++ test_SourceCodes/sandbox_test.py
@@ -73,82 +73,9 @@ df_reviews = df_reviews.drop(df_reviews[df_reviews["Rating"]=='3'].index, inplac
 # Data are already clean, next lines will be your test source code
 # ---------------------------------------------------------------------
 
-#--------------------------------------------------------------------------------------------
-#Extracting phrases for creating corpora that will be use in decision tree recommendation
-#--------------------------------------------------------------------------------------------
+from googletrans import Translator
 
-cursor = mysqldb.cursor()
-cursor.execute("DELETE FROM attribute_table")
-mysqldb.commit()
-    
-df_reviews = df_reviews.drop(axis=1, columns=["Date"])
-df = pd.DataFrame()
-
-def extract_attrib(attrib_value):
-  #df_atttribute = df_reviews.loc[df_reviews["Model"].str.contains('Test Phone', regex=False)]
-  df_temp = df_reviews.loc[df_reviews["Reviews"].str.contains(attrib_value, regex=False)]
-  df_temp["Reviews"] = df_temp["Reviews"].str.replace('[0-9]', "", regex=True)
-  
-  if attrib_value == "battery":
-    df_temp["Reviews"] = df_temp["Reviews"].str.extract(r'\b((?:\w+\W+){0,2}battery\b(?:\W+\w+){0,2})')
-  elif attrib_value == "speed":
-    df_temp["Reviews"] = df_temp["Reviews"].str.extract(r'\b((?:\w+\W+){0,2}speed\b(?:\W+\w+){0,2})')
-  elif attrib_value == "memory":
-    df_temp["Reviews"] = df_temp["Reviews"].str.extract(r'\b((?:\w+\W+){0,2}memory\b(?:\W+\w+){0,2})')
-  elif attrib_value == "screen":
-    df_temp["Reviews"] = df_temp["Reviews"].str.extract(r'\b((?:\w+\W+){0,2}screen\b(?:\W+\w+){0,2})')
-  else:
-    df_temp["Attribute"] = attrib_value
-  df_temp = df_temp.dropna(axis=0, subset=["Reviews"], how='any')
-  df_temp = df_temp.drop_duplicates(subset="Reviews")
-  df_temp["Attribute"] = attrib_value
-  return df_temp
-
-list_attrib = ["battery", "screen", "speed", "memory"]
-
-for attrib in list_attrib:
-  df = pd.concat([df, extract_attrib(attrib)])
-  #print("\n Value attribute is: ", attrib)
-  #df.to_csv(attrib+"_All_corpus.csv", index=False)
-
-attrib_matrix = pd.DataFrame(columns=["Model", "Batt_PR","Batt_NR", "Scr_PR", "Scr_NR", "Spd_PR", "Spd_NR", "Mem_PR", "Mem_NR"])
-gadget_list = distinct_value = df_reviews["Model"].unique()
-
-def convert_to_matrix(gadget_model):
-  df_model = df.loc[df["Model"].str.contains(gadget_model)]
-  df_rev = df_model.loc[df_model["Reviews"].str.contains("battery")]
-  batt_rpos = df_rev["Rating"].value_counts().get("1",0)
-  batt_rneg = df_rev["Rating"].value_counts().get("0",0)
-
-  df_rev = df_model.loc[df_model["Reviews"].str.contains("screen")]
-  scr_rpos = df_rev["Rating"].value_counts().get("1",0)
-  scr_rneg = df_rev["Rating"].value_counts().get("0",0)
-
-  df_rev = df_model.loc[df_model["Reviews"].str.contains("speed")]
-  spd_rpos = df_rev["Rating"].value_counts().get("1",0)
-  spd_rneg = df_rev["Rating"].value_counts().get("0",0)
-
-  df_rev = df_model.loc[df_model["Reviews"].str.contains("memory")]
-  mem_rpos = df_rev["Rating"].value_counts().get("1",0)
-  mem_rneg = df_rev["Rating"].value_counts().get("0",0)
-
-  row_value = [gadget_model, batt_rpos, batt_rneg, scr_rpos, scr_rneg, spd_rpos, spd_rneg, mem_rpos, mem_rneg]
-  return row_value
-
-for colname in gadget_list:
-  attrib_matrix.loc[len(attrib_matrix)] = convert_to_matrix(colname)
-
-attrib_matrix.to_sql(con=engine, name="attribute_table", if_exists='replace', index=False)
-engine.dispose()
-
-import openai
-openai.api_key = "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"
-response = openai.Image.create(
-    prompt="image of dog flying",
-    n=1,
-    size="256x256"
-)
-image_url = response['data'][0]['url']
-print(image_url)
-
-
+translator = Translator()
+str_to_translate = "Ang daling ikabit at zero so walang galawan at hindi prone magasgas ang mismong camera lenses.  na sulit kasi ang mura din"
+translated_string = translator.translate(str_to_translate, src="auto", dest="en")
+print ("Converted Value:" + translated_string.text)
